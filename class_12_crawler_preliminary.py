eb974e569716cba05e078b5dd833216537c32e7b
caoshuai <iscshuai@163.com>
2021-07-08 11:39:04 +0800

    class 12 crawler -- Bilibili rank crawler

--- class_12_crawler_preliminary.py
+++ class_12_crawler_preliminary.py
@@ -18,8 +18,9 @@ class MyCrawler:
 	# 3. 存——保存采集结果
 	def save(self, info):
 		with open(self.filename, 'w', encoding='utf-8') as f:
+			f.write('\t'.join(['链接', '标题', '播放量', '弹幕数', 'UP主', '综合得分']) + '\n')
 			for res in info:
-				f.write(res[0] + '\t' + res[1] + '\n')
+				f.write('\t'.join(res) + '\n')
 
 	def crawler(self, url, pattern):
 		content = self.download(url)
@@ -27,10 +28,18 @@ class MyCrawler:
 		self.save(info)
 
 
-filename = './crawler-media/mobile.txt'
-url = 'https://wap.zol.com.cn/top/cell_phone/hot.html'
-pattern = '<p class="pro-info-name f28">(.*?)</p>[\S\s]*?<span class="pro-info-price f24">(.*?)</span>'
+# filename = './crawler-media/mobile.txt'
+# url = 'https://wap.zol.com.cn/top/cell_phone/hot.html'
+# pattern = '<p class="pro-info-name f28">(.*?)</p>[\S\s]*?<span class="pro-info-price f24">(.*?)</span>'
+
+filename = './crawler-media/bilibili_rank.txt'
+url = 'https://www.bilibili.com/v/popular/rank/all'
+pattern = '<div class="info"><a href="\/\/(.*?)" target="_blank" class="title">(.*?)<\/a>.*?<\/i>\s+(.*?)\s+.*?<\/i>\s+(.*?)\s+.*?<\/i>\s+(.*?)\s+.*?<div class="pts"><div>(\d+)<\/div>'
 
 mycrawler = MyCrawler(filename=filename)
 
-mycrawler.crawler(url, pattern)
+content = mycrawler.download(url)
+info = mycrawler.extract(content, pattern)
+mycrawler.save(info)
+
+# mycrawler.crawler(url, pattern)

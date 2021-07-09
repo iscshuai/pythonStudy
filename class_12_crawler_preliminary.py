848c072a78f38e4430d2ab885801a08c6746678b
caoshuai <iscshuai@163.com>
2021-07-09 09:30:14 +0800

    class 12 crawler -- Bilibili rank crawler

--- class_12_crawler_preliminary.py
+++ class_12_crawler_preliminary.py
@@ -4,10 +4,13 @@ import re
 class MyCrawler:
 	def __init__(self, filename):
 		self.filename = filename
+		self.headers = {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+		}
 
 	# 1. 采——网页的采集
 	def download(self, url):
-		req = requests.get(url)
+		req = requests.get(url, headers=self.headers)
 		return req.text
 
 	# 2. 抽——信息的抽取
@@ -32,6 +35,12 @@ class MyCrawler:
 # url = 'https://wap.zol.com.cn/top/cell_phone/hot.html'
 # pattern = '<p class="pro-info-name f28">(.*?)</p>[\S\s]*?<span class="pro-info-price f24">(.*?)</span>'
 
+# mycrawler = MyCrawler(filename=filename)
+# mycrawler.crawler(url, pattern)
+
+'''
+爬取bilibili rank
+'''
 filename = './crawler-media/bilibili_rank.txt'
 url = 'https://www.bilibili.com/v/popular/rank/all'
 pattern = '<div class="info"><a href="\/\/(.*?)" target="_blank" class="title">(.*?)<\/a>.*?<\/i>\s+(.*?)\s+.*?<\/i>\s+(.*?)\s+.*?<\/i>\s+(.*?)\s+.*?<div class="pts"><div>(\d+)<\/div>'
@@ -42,4 +51,4 @@ content = mycrawler.download(url)
 info = mycrawler.extract(content, pattern)
 mycrawler.save(info)
 
-# mycrawler.crawler(url, pattern)
+### mycrawler.crawler(url, pattern)

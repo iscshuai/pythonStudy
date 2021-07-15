3e60f71536d7a55d144c0859bd14148a10d6a6af
caoshuai <iscshuai@163.com>
2021-07-15 09:44:50 +0800

    class 14 crawler --Use xpath to extract douban book info

--- class_14_crawler_xpath.py
+++ class_14_crawler_xpath.py
@@ -1,5 +1,6 @@
 from class_13_crawler_douban import MyCrawler
 from lxml import html
+import re
 
 douban_crawler = MyCrawler('douban.txt')
 content = douban_crawler.download('https://book.douban.com/tag/%E7%A5%9E%E7%BB%8F%E7%BD%91%E7%BB%9C')
@@ -15,7 +16,8 @@ xpath_book_intro = list(map(lambda x: x.text.strip(), tree.xpath("//div[@class='
 book_infos = tree.xpath("//li[@class='subject-item']")
 
 for book_info in book_infos:
-	book_title = book_info.xpath(".//h2/a")[0].text.strip()
+	book_title_elem = book_info.xpath(".//h2/a")[0]
+	book_title = re.sub('\s{2,}', '', book_title_elem.text_content().replace('\n', ''))
 	book_url = book_info.xpath(".//h2/a")[0].attrib['href']
 	book_pub_info = book_info.xpath(".//div[@class='pub']")[0].text.strip()
 	book_intro = 'N/A'
